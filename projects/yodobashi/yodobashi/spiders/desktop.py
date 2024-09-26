03b4f66f294993e6493ad8554385c01d3f67077b
eqs-tagami <eqs.tagami@gmail.com>
2024-09-26 14:43:02 +0900

    ヨドバシ

--- projects/yodobashi/yodobashi/spiders/desktop.py
+++ projects/yodobashi/yodobashi/spiders/desktop.py
@@ -11,23 +11,23 @@ class DesktopSpider(scrapy.Spider):
         # 商品取得
         products = response.xpath('//div[contains(@class,"productListTile")]')
         # products = response.css('div.productListTile')
-        # for product in products:
-        #     # メーカー名
-        #     # 一度取得したproductsのデータを扱うので、「.」を使う必要がある
-        #     maker = product.xpath('.//div[contains(@class,"pName")]/p/text()').get()
-        #     # maker = product.css('div.pName > p::text')
+        for product in products:
+            # メーカー名
+            # 一度取得したproductsのデータを扱うので、「.」を使う必要がある
+            maker = product.xpath('.//div[contains(@class,"pName")]/p/text()').get()
+            # maker = product.css('div.pName > p::text')
 
-        #     # 商品名
-        #     name = product.xpath('.//div[contains(@class,"pName")]/p[2]/text()').get()
+            # 商品名
+            name = product.xpath('.//div[contains(@class,"pName")]/p[2]/text()').get()
 
-        #     # 価格
-        #     price = product.xpath('.//span[@class="productPrice"]/text()').get()
+            # 価格
+            price = product.xpath('.//span[@class="productPrice"]/text()').get()
 
-        #     yield {
-        #         'maker': maker,
-        #         'name': name,
-        #         'price': price
-        #     }
+            yield {
+                'maker': maker,
+                'name': name,
+                'price': price
+            }
         next_page = response.xpath('//a[@class="next"]/@href').get()
         if next_page:
             yield response.follow(url=next_page, callback=self.parse)
