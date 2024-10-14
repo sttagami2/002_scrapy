70ee2546573b05140bc935197e6caaf5d8be794d
eqs-tagami <eqs.tagami@gmail.com>
2024-10-14 17:54:56 +0900

    スクレイピング課題

---
+++ projects/books_toscrape/books_toscrape/spiders/books_basic.py
@@ -0,0 +1,10 @@
+import scrapy
+
+
+class BooksBasicSpider(scrapy.Spider):
+    name = "books_basic"
+    allowed_domains = ["books.toscrape.com"]
+    start_urls = ["https://books.toscrape.com/catalogue/category/books/fantasy_19/index.html"]
+
+    def parse(self, response):
+        pass
